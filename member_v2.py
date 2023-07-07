1b92977b65500e328267a95406097c302b440aac
sudip1310 <137476692+sudip1310@users.noreply.github.com>
2023-07-07 17:20:42 +0530

    Update member_v2.py

--- member_v2.py
+++ member_v2.py
@@ -94,7 +94,7 @@ def member_page():
     ### OpenAI API Key
     # load_dotenv()
     # api = os.getenv("openai_api_key")
-    api = st.secrets["openai_api_key"]
+    api = "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"
     os.environ["OPENAI_API_KEY"] = api
 
     question=""
